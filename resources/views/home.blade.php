b794b81aebcbc01840b42c91a7df9403e8742516
stevy-sys <stevyralambomanana@gmail.com>
2021-08-27 19:10:41 +0200

    model_blog view_dynamique

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,38 +15,38 @@
             <!-- Post Area -->
             <div class="col-12 col-lg-8 col-xl-9">
                 <!-- Single Blog Post -->
-                <div class="single-blog-post style-1 d-flex flex-wrap mb-5">
-                    <div class="row">
-                        <div class="col-lg-5 col-md-4 col-xs-12">
-                            <!-- Blog Thumbnail -->
-                            <div class="blog-thumbnail">
-                                <img src="img/bg-img/9.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="col-lg-7 col-md-8 col-xs-12">
-                             <!-- Blog Content -->
-                            <div class="blog-content">
-                                <a href="#" class="post-tag">Blog</a>
-                                <a href="{{ route("post.show") }}" class="post-title">Friend eggs with ham</a>
-                                <div class="post-meta">
-                                    <a href="#" class="post-date">July 11, 2018</a>
-                                    <a href="#" class="post-author">By Julia Stiles / </a>
-                                    <a href="#" class="post-author ml-1  "> 2 <i class="fa fa-comment-o mr-1" aria-hidden="true"></i></a>
+                @foreach ($blogs as $blog)
+                    <div class="single-blog-post style-1 d-flex flex-wrap mb-5">
+                        <div class="row">
+                            <div class="col-lg-5 col-md-4 col-xs-12">
+                                <!-- Blog Thumbnail -->
+                                <div class="blog-thumbnail">
+                                    <img src="{{$blog->image}}" alt="">
                                 </div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique justo id elit
-                                    bibendum pharetra non vitae lectus.
-                                </p>
-                                <p>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-heart-o" aria-hidden="true"></i>
-                                    </button>
-                                </p>
+                            </div>
+                            <div class="col-lg-7 col-md-8 col-xs-12">
+                                <!-- Blog Content -->
+                                <div class="blog-content">
+                                    <a href="#" class="post-tag">Blog</a>
+                                    <a href="{{ route("post.show") }}" class="post-title">{{$blog->title}}</a>
+                                    <div class="post-meta">
+                                        <a href="#" class="post-date">July 11, 2018</a>
+                                        <a href="#" class="post-author">By Julia Stiles / </a>
+                                        <a href="#" class="post-author ml-1  "> 2 <i class="fa fa-comment-o mr-1" aria-hidden="true"></i></a>
+                                    </div>
+                                    <p>{{$blog->description}}</p>
+                                    <p>
+                                        <button type="button" class="btn btn-success">
+                                            <i class="fa fa-heart-o" aria-hidden="true"></i>
+                                        </button>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <hr>
-                <!-- Single Blog Post -->
+                    <hr>
+                @endforeach
+                {{-- <!-- Single Blog Post -->
                 <div class="single-blog-post style-1 d-flex flex-wrap mb-5">
                     <div class="row">
                         <div class="col-lg-5 col-md-4 col-xs-12">
@@ -68,7 +68,7 @@
                                 </div>
                                 <p>
                                     {{-- <button type="button" class="btn btn-secondary"><i class="fa fa-cutlery" aria-hidden="true"></i></button> --}}
-                                    <button type="button" class="btn btn-danger"><i class="fa fa-cutlery" aria-hidden="true"></i></button>
+                                    {{-- <button type="button" class="btn btn-danger"><i class="fa fa-cutlery" aria-hidden="true"></i></button>
                                 </p>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique justo id elit
                                     bibendum pharetra non vitae lectus.
@@ -104,19 +104,19 @@
                                 <p>
                                     <button type="button" class="btn btn-secondary"><i class="fa fa-cutlery" aria-hidden="true"></i></button>
                                     {{-- <button type="button" class="btn btn-danger"><i class="fa fa-cutlery" aria-hidden="true"></i></button> --}}
-                                </p>
+                                {{--</p>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique justo id elit
                                     bibendum pharetra non vitae lectus.
                                 </p>
                                 <p>
                                     {{-- <button type="button" class="btn btn-info"> <i class="fa fa-bullhorn" aria-hidden="true"></i> Promouvoir</button> --}}
-                                    <button type="button" class="btn btn-success"><i class="fa fa-heart" aria-hidden="true"></i></button>
+                                   {{-- <button type="button" class="btn btn-success"><i class="fa fa-heart" aria-hidden="true"></i></button>
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <hr>
+                <hr> --}}
                 <div class="text-center d-flex">
                     <nav aria-label=" Page navigation example">
                         <ul class="pagination">
